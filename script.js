209c9423dfc39c4caa6df2240126ea707c2290a2
Steven McWee <stevenmcwee@gmail.com>
2025-06-05 16:17:59 +0100

    Start styling the calc and add number buttons

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Operations
 function add(a, b) {
     return a + b;
 }
@@ -14,6 +15,7 @@ function divide(a, b) {
     return a / b;
 }
 
+// Calculation
 let num1 = 0;
 let num2 = 0;
 let operator = '+';
@@ -33,4 +35,17 @@ function operaate(num1, operator, num2) {
             divide(num1, num2);
             break;
     };
+};
+
+// Set up buttons
+const numberPad = document.querySelector('.number-pad');
+
+for (let i = 0; i < 10; i++) {
+    const button = document.createElement('button');
+    button.classList.add('number-button');
+    button.textContent = i;
+    button.addEventListener('click', () => {
+        console.log(i);
+    })
+    numberPad.appendChild(button);
 };

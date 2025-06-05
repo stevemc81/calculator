46b2e81131138019c9fe662192103ce0e26b1b8c
Steven McWee <stevenmcwee@gmail.com>
2025-06-05 14:49:14 +0100

    Add function to perform correct operation

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Basic operations
 function add(a, b) {
     return a + b;
 }
@@ -14,3 +13,24 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let num1 = 0;
+let num2 = 0;
+let operator = '+';
+
+function operaate(num1, operator, num2) {
+    switch (operator) {
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;
+    };
+};
